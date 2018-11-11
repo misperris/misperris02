d8143de84a47f05070366a627a32d4ba48c70148
misperris <misperrisproyecto@gmail.com>
2018-11-11 06:51:05 -0300

    commit7

--- MisPerris/settings.py
+++ MisPerris/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,3 +128,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+LOGIN_REDIRECT_URL = reverse_lazy('adopcion_solicitud_listar')
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = 'misperrisproyecto@gmail.com'
+EMAIL_HOST_PASSWORD = 'MisPerris01'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
